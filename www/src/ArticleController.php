1999992c70267a773bf20fc28ad39610cdb0914d
kry-ign <kris_ignatiuk@wp.pl>
2022-01-30 20:52:21 +0100

    commit

---
+++ www/src/ArticleController.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+require_once("AbstractController.php");
+
+use App\Exception\NotFoundException;
+
+class Controller extends AbstractController
+{
+    public function createAction()
+    {
+        if ($this->request->hasPost()) {
+            $this->database->createArticle([
+                'title' => $this->request->postParam('title'),
+                'description' => $this->request->postParam('description'),
+            ]);
+            header('Location: /?before=created');
+        }
+        $this->view->render('create',);
+    }
+
+    public function listAction()
+    {
+        $this->view->render(
+            'list',
+            [
+                'notes' => $this->database->getArtilces(),
+                'before' => $this->request->getParam('before'),
+                'error' => $this->request->getParam('error')
+            ]
+            );
+    }
+
+    public function showAction()
+    {
+        $noteId = (int)$this->request->getParam('id');
+        if (!$noteId) {
+            header('Location /?error=missingArticleId');
+            exit;
+        }
+        try {
+            $note = $this->database->getArticle($noteId);
+        } catch (NotFoundException $e) {
+            header('Location /?error=articleNotFound');
+            exit;
+        } catch (Exception\StorageException $e) {
+        }
+        $this->view->render(
+            'show',
+            ['note' => $note]
+        );
+    }
+
+}
